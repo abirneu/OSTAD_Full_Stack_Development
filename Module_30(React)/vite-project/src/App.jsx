54553eb4a369e23964d0f646c532e9011a2d7440
abirneu <abirhasan.shu@gmail.com>
2025-08-28 16:51:27 +0600

    run true

--- Module_30(React)/vite-project/src/App.jsx
+++ Module_30(React)/vite-project/src/App.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 
 const App = () => {
-  const user = {isAdmin : true}
+  // const user = {isAdmin : true}
+  // return(
+  //   <div>
+  //     <p>Hello abir</p>
+  //     {user.isAdmin ? <p>You are admin</p>: <p>you are user</p>}
+  //   </div>
+  // )
+  const isLoggedIn = true
   return(
     <div>
-      <p>Hello abir</p>
-      {user.isAdmin ? <p>You are admin</p>: <p>you are user</p>}
+      <p>Hello</p>
+      {isLoggedIn && <h1>Welcome Back</h1>}
     </div>
   )
   
     
-
-
-     
-
-  
 };
 
 export default App;
